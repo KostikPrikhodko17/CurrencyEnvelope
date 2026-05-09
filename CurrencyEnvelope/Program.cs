5f1c1a50ad2b51bf79d3a192666a6cac07e8f889
kostik <kostik.prikhodko2007@gmail.com>
2026-05-09 22:03:06 +0300

    Добавлена возможность конвертации в обе стороны.

--- CurrencyEnvelope/Program.cs
+++ CurrencyEnvelope/Program.cs
@@ -11,36 +11,59 @@ namespace CurrencyEnvelope
         static void Main(string[] args)
         {
             // 2 добавить больше валют
-            // 3 добавить возможность конвертации в обе стороны (например, 1. TRY <> RUB и 1. RUB <> TRY)
             // 4. добаваиь цикл для повторного использования программы без перезапуска
             // 5. добавить обработку ошибок для некорректного ввода (например, если пользователь вводит не число для суммы конвертации)
             // 6. Улучшить switch-case для обработки выбора валюты, чтобы можно было легко добавлять новые валюты в будущем (например, использовать словарь для хранения курсов валют и их названий)
+            // 7. В отображение результата добавить какую валюту пользователь получил после конвертации, а не только сумму
 
 
 
-            string currency = "1. TRY <> RUB";
+            string[] currency = { "1. TRY < > RUB", "2. USD < > RUB" };
+            double[] priceCurrency = { 1.64, 0.61 };
+
 
             Console.WriteLine("Конвертация валют.");
-            Console.WriteLine("Доступные валюты для конвертации.\n" + currency);
-            Console.Write("\nВыберите валюту для конвертации: ");
+            Console.WriteLine("\nВыберите валюту для конвертации.");
+
+            for (int i = 0; i <= currency.Length - 1; i++)
+            {
+                Console.WriteLine(currency[i]);
+            }
 
             string userChoice = Console.ReadLine();
 
             switch (userChoice)
             {
                 case "1":
-                    double liraInRub = 1.64;
-                    double result = ConvertCurrency(liraInRub);
-                    Console.WriteLine($"Результат конвертации: {result} RUB");
+                    double correctPrice = ReturnCurrency(currency, userChoice, priceCurrency);
+                    double result = ConvertCurrency(correctPrice);
+                    Console.WriteLine($"Результат конвертации: {result}");
                     break;
             }
         }
 
-        static double ConvertCurrency(double priceCurrency)
+        static double ConvertCurrency(double priceCurrency) // расчет конвертации валюты
         {
             Console.Write("Введите сумму для конвертации: ");
             double sumConvert = Convert.ToDouble(Console.ReadLine());
             return sumConvert * priceCurrency;
+        }
+
+        static double ReturnCurrency(string[] curruncy, string userChoice, double[] priceCurrency) // определение направления конвертации и возврат правильного курса валюты
+        {
+            double correctPrice;
+            Console.WriteLine("\n" + curruncy[int.Parse(userChoice) - 1]);
+            Console.Write("Выберите направление конвертации: > < : ");
+            string choice = Console.ReadLine();
+            if (choice == ">")
+            {
+                correctPrice = priceCurrency[int.Parse(userChoice) - 1];
+            }
+            else
+            {
+                correctPrice = priceCurrency[int.Parse(userChoice)];
+            }
+            return correctPrice;
         }
     }
 }
