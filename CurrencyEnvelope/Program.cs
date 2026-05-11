817856fdaba0f82a1a336b8749557e437be6d8a1
kostik <kostik.prikhodko2007@gmail.com>
2026-05-11 11:14:16 +0300

    Переработан выбор валюты. Добавлена валюта. Добавлен вывод конвертируемой валюты.

--- CurrencyEnvelope/Program.cs
+++ CurrencyEnvelope/Program.cs
@@ -10,39 +10,36 @@ namespace CurrencyEnvelope
     {
         static void Main(string[] args)
         {
-            // 2 добавить больше валют
             // 4. добаваиь цикл для повторного использования программы без перезапуска
             // 3. добавить возможность пользователю вводить свои собственные курсы валют для конвертации
-            // 1. Добавить доп массиив с номерами валют и улучшить ыцith case с помощью when Contains()
             // 5. добавить обработку ошибок для некорректного ввода (например, если пользователь вводит не число для суммы конвертации)
-            // 6. Улучшить switch-case для обработки выбора валюты, чтобы можно было легко добавлять новые валюты в будущем (например, использовать словарь для хранения курсов валют и их названий)
-            // 7. В отображение результата добавить какую валюту пользователь получил после конвертации, а не только сумму
 
 
 
-            string[] currency = { "1. TRY > RUB", "2. RUB > TRY", "3. USD > RUB", "4. RUB > USD" };
-            double[] priceCurrency = { 1.64, 0.61, 74.24, 0.013 };
+            string[] currency = { "TRY > RUB", "RUB > TRY", "USD > RUB", "RUB > USD", "EUR > RUB", "RUB > EUR" }; 
+            string[] idCurruncy = { "1", "2", "3", "4", "5", "6" };
+            double[] priceCurrency = { 1.64, 0.61, 74.24, 0.013, 87.29, 0.011 }; 
 
 
             Console.WriteLine("Конвертация валют.");
             Console.WriteLine("\nВыберите валюту для конвертации.");
 
-            foreach (string item in currency)
+            for (int i = 0; i < idCurruncy.Length; i++)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(idCurruncy[i] + " - " + currency[i]);
             }
 
             string userChoice = Console.ReadLine();
 
             switch (userChoice)
             {
-                case "1":
-                case "2":
-                case "3":
-                case "4":
+                case string when idCurruncy.Contains(userChoice):
                     double correctPrice = ReturnCurrency(currency, userChoice, priceCurrency);
                     double result = ConvertCurrency(correctPrice);
-                    Console.WriteLine($"Результат конвертации: {result}");
+                    Console.WriteLine($"Результат конвертации: {currency[int.Parse(userChoice) - 1]} = {result}");
+                    break;
+                default:
+                    Console.WriteLine("Некорректный выбор валюты.");
                     break;
             }
         }
